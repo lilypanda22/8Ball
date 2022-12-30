be2b11e4655c7987e0f0b338b6a51733a36f9f1e
lilypanda22 <96503176+lilypanda22@users.noreply.github.com>
2022-12-30 12:30:07 -0600

    Add files via upload
    used an array list instead of a bunch of if statements. I was told that was not ideal.

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 // This is 8Ball on Java, my very first individual project. It seems very simple,
 // but I definitely had to think a lot on how to tie math.random into 20 different answer choices.
 // Now I understand more on how to utilize math.random.
 public class Main {
+    public static final ArrayList<String> results = new ArrayList<>();
+
     public static void main(String[] args) {
+        makeArray();
         getQuestion();
 
         boolean play = true;
@@ -12,6 +16,29 @@ public class Main {
             result();
             getQuestion();
         }
+    }
+
+    public static void makeArray() {
+        results.add("It is certain.\n");
+        results.add("It is decidedly so.\n");
+        results.add("Without a doubt.\n");
+        results.add("Yes definitely.\n");
+        results.add("You may rely on it.\n");
+        results.add("As I see it, yes.\n");
+        results.add("Most likely.\n");
+        results.add("Outlook good.\n");
+        results.add("Yes.\n");
+        results.add("Signs point to yes.\n");
+        results.add("Reply hazy, try again.\n");
+        results.add("Ask again later.\n");
+        results.add("Better not tell you now.\n");
+        results.add("Cannot predict now.\n");
+        results.add("Concentrate and ask again.\n");
+        results.add("Don't count on it.\n");
+        results.add("My reply is no.\n");
+        results.add("My sources say no.\n");
+        results.add("Outlook not so good.\n");
+        results.add("Very doubtful.\n");
     }
 
     public static void getQuestion() {
@@ -22,66 +49,6 @@ public class Main {
 
     public static void result() {
         double number = Math.random() * 20;
-        if ((int)number == 0) {
-            System.out.println("It is certain.\n");
-        }
-        else if ((int)number == 1) {
-            System.out.println("It is decidedly so.\n");
-        }
-        else if ((int)number == 2) {
-            System.out.println("Without a doubt.\n");
-        }
-        else if ((int)number == 3) {
-            System.out.println("Yes definitely.\n");
-        }
-        else if ((int)number == 4) {
-            System.out.println("You may rely on it.\n");
-        }
-        else if ((int)number == 5) {
-            System.out.println("As I see it, yes.\n");
-        }
-        else if ((int)number == 6) {
-            System.out.println("Most likely.\n");
-        }
-        else if ((int)number == 7) {
-            System.out.println("Outlook good.\n");
-        }
-        else if ((int)number == 8) {
-            System.out.println("Yes.\n");
-        }
-        else if ((int)number == 9) {
-            System.out.println("Signs point to yes.\n");
-        }
-        else if ((int)number == 10) {
-            System.out.println("Reply hazy, try again.\n");
-        }
-        else if ((int)number == 11) {
-            System.out.println("Ask again later.\n");
-        }
-        else if ((int)number == 12) {
-            System.out.println("Better not tell you now.\n");
-        }
-        else if ((int)number == 13) {
-            System.out.println("Cannot predict now.\n");
-        }
-        else if ((int)number == 14) {
-            System.out.println("Concentrate and ask again.\n");
-        }
-        else if ((int)number == 15) {
-            System.out.println("Don't count on it.\n");
-        }
-        else if ((int)number == 16) {
-            System.out.println("My reply is no.\n");
-        }
-        else if ((int)number == 17) {
-            System.out.println("My sources say no.\n");
-        }
-        else if ((int)number == 18) {
-            System.out.println("Outlook not so good.\n");
-        }
-        else if ((int)number == 19) {
-            System.out.println("Very doubtful.\n");
-        }
-        else System.out.println((int)number + "\n");
+        System.out.println(results.get((int)number));
     }
 }
